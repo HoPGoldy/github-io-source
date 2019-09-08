e28b6ed69454417463cdb73ef8bae75a7b3dece9
huang puguang <puguang_huang@fpi-inc.com>
2019-09-08 17:47:19 +0800

    fix: 复制命令错误

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -9,7 +9,7 @@ yarn build
 
 echo "复制到 github.io"
 rm -rf /home/auto-deploy-repository/HoPGoldy.github.io/**
-copy /hom/auto-deploy-repository/github-io-source/dist/** /home/auto-deploy-repository/HoPGoldy.github.io/
+cp -r /home/auto-deploy-repository/github-io-source/dist/** /home/auto-deploy-repository/HoPGoldy.github.io/
 
 echo "提交 github.io"
 cd /home/auto-deploy-repository/HoPGoldy.github.io/
